d55dc9866ab4edca435ebeb80290448cc588d823
abasslinelow <abasslinelow@gmail.com>
2021-01-06 14:01:00 -0500

    Pipes now attach to each other correctly

---
+++ Assets/Scripts/Plumbing/DisableGrabIfSnapped.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Linq;
+
+public class DisableGrabIfSnapped : MonoBehaviour
+{
+    XRSocketInteractor socket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket = gameObject.GetComponent<XRSocketInteractor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (socket.selectTarget == null) {
+            socket.transform.parent.GetComponent<MeshCollider>().enabled = true;
+        } else {
+            socket.transform.parent.GetComponent<MeshCollider>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Plumbing/Interfaces.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public interface IAttachable {
+    void Attach();
+}
+
+public class SomeClass {
+    public T GenericMethod<T>(T param) where T : IAttachable {
+        return param;
+    }
+}
